a6117c128850aab802f94c17e3157a53ae1f8134
antore00 <antonio.reccchia.0@gmail.com>
2025-11-23 12:48:51 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Benvenuto in SkillSell 🚀</h1>
+      <p>Firebase è collegato correttamente.</p>
+    </div>
+  );
+}
