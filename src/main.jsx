5cb817f99d4d6d3c5ff2ef873b01c1c513c9b0a9
antore00 <antonio.reccchia.0@gmail.com>
2025-11-23 13:44:45 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-function App() {
-  return (
-    <div style={{ fontFamily: "sans-serif", padding: "40px" }}>
-      <h1>Benvenuto in SkillSell 🚀</h1>
-      <p>L’app è online e funziona.</p>
-      <p>Da qui inizieremo ad aggiungere tutte le funzionalità del marketplace.</p>
-    </div>
-  )
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
